b980c8f42c878a43dd50d21bb38b6edb6cbf7172
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-01-15 14:24:01 -0300

    layout.tsx NavBar & children z-index

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,10 +6,11 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
 
       <div className="flex">
-
+        <div className='z-20'>
         <NavBar />
+        </div>
 
-        <div className="p-2 w-full text-slate-900 sm2:mt-16 sm2:ml-0 sm:ml-80">
+        <div className="z-10 p-2 w-full text-slate-900 sm2:mt-16 sm2:ml-0 sm:ml-80">
           { children }
         </div>
 
