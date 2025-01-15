4bf40ce090a90d83b8fe1b503199054ca5136422
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-01-15 13:45:55 -0300

    SideBarMenuItem component closeFullMenu modal

--- src/components/ui/FullMenu.tsx
+++ src/components/ui/FullMenu.tsx
@@ -36,9 +36,8 @@ const menuItems = [
 ];
 
 export const FullMenu = () => {
-  const isFullMenuOpen = useUIStore((state) => state.isFullMenuOpen)
+  const isFullMenuOpen = useUIStore((state) => state.isFullMenuOpen);
   const closeFullMenu = useUIStore((state) => state.closeFullMenu);
-
 
   return (
     <div id="menu">
@@ -46,38 +45,37 @@ export const FullMenu = () => {
         // style={{ width: "screen" }}
         className="fixed bg-gray-900 min-h-screen min-w-full text-slate-300 w-64 left-0 overflow-y-scroll"
       >
-        <div id="logo" className="my-4 px-6">
-          <h1 className="flex items-center  text-lg md:text-2xl font-bold text-white">
-            Poke Api
-            <span className="text-blue-500">
-              <RiNextjsLine className="ml-4" size={30} />
-            </span>
-          </h1>
-          <p className="text-slate-500 text-sm">
-            Lista de todos los pokémon de Kanto
-          </p>
+        <div className="flex">
+          <div id="logo" className="my-4 px-6">
+            <h1 className="flex items-center  text-lg md:text-2xl font-bold text-white">
+              Poke Api
+              <span className="text-blue-500">
+                <RiNextjsLine className="ml-4" size={30} />
+              </span>
+            </h1>
+            <p className="text-slate-500 text-sm">
+              Lista de todos los pokémon de Kanto
+            </p>
 
-          <p className="text-slate-500 text-sm columns-1 flex-col">
-            Poke Api hecha gracias a
-          </p>
-          <Link className="underline" href={"https://pokeapi.co"}>
-            https://pokeapi.co/
-          </Link>
+            <p className="text-slate-500 text-sm columns-1 flex-col">
+              Poke Api hecha gracias a
+            </p>
+            <Link className="underline" href={"https://pokeapi.co"}>
+              https://pokeapi.co/
+            </Link>
+          </div>
+
+          <button className="top-10 ml-56" onClick={closeFullMenu}>
+            <IoCloseCircleOutline size={70} />
+          </button>
         </div>
 
         <div id="nav" className="w-full px-6 mt-14">
           {menuItems.map((item) => (
-            <div>
-              <SideBarMenuItem key={item.path} {...item} />
+            <div key={item.path} onClick={closeFullMenu}>
+              <SideBarMenuItem {...item} />
             </div>
           ))}
-        </div>
-
-        <div className="flex m-auto" >
-          <button onClick={closeFullMenu}>
-
-          <IoCloseCircleOutline />
-          </button>
         </div>
       </div>
     </div>
