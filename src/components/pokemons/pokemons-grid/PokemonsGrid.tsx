f465d1b13828ccbf1f95dcc00f63ea452a71a6f2
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-01-16 12:24:56 -0300

    Search component

--- src/components/pokemons/pokemons-grid/PokemonsGrid.tsx
+++ src/components/pokemons/pokemons-grid/PokemonsGrid.tsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from "react";
 import { PokemonsResponse, SimplePokemon } from "@/interfaces";
-import Image from 'next/image';
-import { PokemonCard } from '@/components';
-
+import Image from "next/image";
+import { PokemonCard } from "@/components";
+import { Search } from "@/components/ui/Search";
 
 interface Props {
   pokemons: SimplePokemon[];
 }
 
-const totalPokemons = 1025
+const totalPokemons = 1025;
 
 export const PokemonsGrid = ({ pokemons }: Props) => {
   return (
-    <div className="flex flex-wrap gap-10 items-center justify-center">
-
-        {
-          pokemons.map( pokemon => (
-            <PokemonCard key={ pokemon.id } pokemon={pokemon} />            
-          ))
-        }
-        
+    <div>
+      <Search />
+      <div className="flex flex-wrap gap-10 items-center justify-center mt-5">
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
---
+++ src/components/ui/Search.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+export const Search = () => {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+
+
+  // # Search to URL Params function
+  const handleSearch = async () => {
+    if (!query) return;
+
+    setLoading(true);
+    try {
+      router.push(`/pokemons/${query.toLowerCase()}`);
+    } catch (error) {
+      console.error("Error al buscar el pokémon:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  return (
+
+    <div className="flex justify-center ">
+    <input
+      type="text"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      placeholder={"Buscar pokémon... Nombre o ID"}
+      className="w-60 text-center "
+      />
+      <button
+      className=""
+      onClick={handleSearch}
+      disabled={loading}>
+        {loading ? "Buscando..." : "Buscar"}
+      </button>
+  </div>
+
+)};
