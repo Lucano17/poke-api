6b4630f38ba1a7a89cd62e52a14685ef8476e301
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-01-20 17:56:55 -0300

    PokemonPage responsive

--- src/app/(dashboard)/pokemons/[name]/page.tsx
+++ src/app/(dashboard)/pokemons/[name]/page.tsx
@@ -77,7 +77,8 @@ export default async function PokemonPage({ params }: Props) {
   const pokemon = await getPokemon(name) ;
   return (
     <div className="flex mt-5 flex-col items-center text-slate-800">
-      <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border  shadow-lg  p-3">
+      <div className="relative flex flex-col items-center rounded-[20px] w-[700px] sm2:w-[550px] 
+      mx-auto bg-white bg-clip-border  shadow-lg  p-3">
         <div className="mt-2 mb-8 w-full">
           <h1 className="px-2 text-2xl font-bold text-slate-700 capitalize">
             #{pokemon.id} {pokemon.name}
@@ -164,7 +165,7 @@ export default async function PokemonPage({ params }: Props) {
           </div>
         </div>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col sm2:w-[500px]  mt-10">
               <h2 className="font-bold text-xl m-auto">Movimientos</h2>
               <div className="flex flex-wrap">
                 <ul className="flex flex-wrap">
