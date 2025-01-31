30502fea97d04f32d30390291f801dc3f292c954
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-01-30 22:15:18 -0300

    FullMenu component responsive styles

--- src/components/ui/FullMenu.tsx
+++ src/components/ui/FullMenu.tsx
@@ -53,27 +53,27 @@ export const FullMenu = () => {
         // style={{ width: "screen" }}
         className="fixed bg-gray-900 min-h-screen min-w-full text-slate-300 w-64 left-0 overflow-y-scroll"
       >
-        <div className="flex">
-          <div id="logo" className="my-4 px-6">
-            <h1 className="flex items-center  text-lg md:text-2xl font-bold text-white">
+        <div className="flex justify-center gap-5">
+          <div id="logo" className="flex flex-col my-4 ml-10 min-w-[200px]">
+            <h1 className="flex items-center text-xl sm2:text-3xl font-bold text-white">
               Poke Api
               <span className="text-blue-500">
-                <RiNextjsLine className="ml-4" size={30} />
+                <RiNextjsLine className="ml-4" size={60} />
               </span>
             </h1>
-            <p className="text-slate-500 text-sm">
+            <p className="text-slate-500 text-lg">
               Lista de todos los pokémon de Kanto
             </p>
 
-            <p className="text-slate-500 text-sm columns-1 flex-col">
+            <p className="text-slate-500 text-md columns-1 flex-col">
               Poke Api hecha gracias a
             </p>
-            <Link className="underline" href={"https://pokeapi.co"}>
+            <Link className="underline text-md" href={"https://pokeapi.co"}>
               https://pokeapi.co/
             </Link>
           </div>
 
-          <button className="top-10 ml-56" onClick={closeFullMenu}>
+          <button className="top-10 mr-10" onClick={closeFullMenu}>
             <IoCloseCircleOutline size={70} />
           </button>
         </div>
